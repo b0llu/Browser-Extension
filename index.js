c4242e7b77aed7dab50a8c7fccfa454db3913082
Dhruv <samantdhruv@gmail.com>
2021-09-11 14:50:10 +0530

    update

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 let myLeads = []
-let inputEl = document.getElementById("iput-el")
-let inputBtn = document.getElementById("input-btn") 
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
-    console.log("Button clicked form addEventListner")
+    myLeads.push(inputEl.value)
+    inputEl.value = "" // creates blank space after input is done
+    renderLeads()
 })
+
+function renderLeads() {
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
+    listItems += `<li>
+    <a target = '_blank' href='${myLeads[i]}'>
+    ${myLeads[i]} 
+    </a>
+    </li>`
+}
+ulEl.innerHTML = listItems
+}
