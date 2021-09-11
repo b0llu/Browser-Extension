faa2eedca0f93dcc9225f4c73d6e7758f21f4ad4
Dhruv <samantdhruv@gmail.com>
2021-09-11 12:12:49 +0530

    update

---
+++ index.js
@@ -0,0 +1,7 @@
+let myLeads = []
+let inputEl = document.getElementById("iput-el")
+let inputBtn = document.getElementById("input-btn") 
+
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked form addEventListner")
+})
