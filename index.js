2c3d966f42428d02ce4c725ab6f8339972f479a2
Dhruv <samantdhruv@gmail.com>
2021-09-11 23:09:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = "" // creates blank space after input is done
