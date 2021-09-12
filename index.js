44cfdcda45bf97c8531d05cfee3cdf62c36d0681
Dhruv <samantdhruv@gmail.com>
2021-09-12 10:07:49 +0530

    added delete button

--- index.js
+++ index.js
@@ -2,12 +2,21 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads() // saves the bookmarked tabs even after restart or refresh of page
 }
+
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    renderLeads()
+})
+
+
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
